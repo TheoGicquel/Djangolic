740aefd4ce608bb2ad2ae75e4911d57f99b4b294
Fabien Rivet <f.rivetdurzy@gmail.com>
2022-10-13 15:09:54 +0200

    add models.py

--- Gueze/models.py
+++ Gueze/models.py
@@ -1,3 +1,57 @@
 from django.db import models
+from django.utils.html import format_html
+import datetime
+
 
 # Create your models here.
+
+
+class Verre(models.Model):
+    id = models.AutoField(primary_key=True, unique=True)
+    name = models.CharField(max_length=100)
+    create_date = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+
+
+
+class srm(models.Model):
+    name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name + " " + self.last_name
+
+
+class available(models.Model):
+    name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name + " " + self.last_name
+
+
+class style(models.Model):
+    name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name + " " + self.last_name
+
+
+class Categorie(models.Model):
+    name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name + " " + self.last_name
+
+
+class bierre(models.Model):
+    name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name + " " + self.last_name
