b17e38b90874fe9536aec15952400157f47db8bf
theogicquel <theo.gicquel.work@gmail.com>
2022-11-26 11:01:20 +0100

    Fix merge conflict

--- Gueze/models.py
+++ Gueze/models.py
@@ -1,28 +1,59 @@
 from django.db import models
-from django.utils.html import format_html
-import datetime
-
-
-# Create your models here.
-
-
 
 class Glass(models.Model):
     id = models.AutoField(primary_key=True, unique=True)
     name = models.CharField(max_length=100)
 
-    def __str__(self):
+    def __unicode__(self):
+        return self.name
+
+class glass(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=5000)
+    asset_link = models.CharField(max_length=5000)
+
+    def __unicode__(self):
+        return self.name
+
+class HexColor(models.Model):
+    hex = models.CharField(max_length=10)
+
+    def __unicode__(self):
+        return self.hex
+
+class srm(models.Model):
+    id = models.AutoField(primary_key=True)
+    color = models.CharField(max_length=100)
+    hex = models.ForeignKey(HexColor, on_delete=models.CASCADE)
+
+    def __unicode__(self):
+        return self.color
+
+class location(models.Model):
+    id = models.AutoField(primary_key=True)
+
+    name = models.CharField(max_length=100)
+    country = models.CharField(max_length=255)
+
+    def __unicode__(self):
         return self.name
 
 
 
+class brewery(models.Model):
+    id = models.AutoField(primary_key=True)
 
-class srm(models.Model):
-    name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=5000)
+    website = models.CharField(max_length=5000)
+    update_at = models.DateField()
+    location_brewery = models.ForeignKey(location, on_delete=models.CASCADE)
 
-    def __str__(self):
-        return self.name + " " + self.last_name
+    def __unicode__(self):
+        return self.name
+
+
 
 
 # general type of beer i.e lager, stout, etc..
@@ -58,7 +89,6 @@ class Beer(models.Model):
     label_medium = models.CharField(max_length=2048,blank=True)
     label_large = models.CharField(max_length=2048,blank=True)
 
-    
 
     def __str__(self):
         return self.name + " " + self.desc
--- README.md
+++ README.md
@@ -1,2 +1,64 @@
 # Djangolic
-A django beverage crud website
+# A django beverage crud website
+
+
+## Installation venv
+```
+virtualenv venv
+```
+
+## Activer le venv
+```
+source venv/bin/activate
+```
+
+## Accéder au bon dossier pour lancer l'application
+Se diriger dans le dossier suivant : /djangolic
+
+## Effectuer les installations nécéssaire
+Installer django :
+```
+pip install django
+```
+
+Installer django-crispy-forms :
+```
+pip install django-crispy-forms
+```
+
+Installer les librairies nécéssaire :
+    - Se déplacer dans le dossier : /djangolic/Gueze/Static
+    - Taper la commande :
+```
+yarnkpg add bootstrap jquery-slim @popperjs/core
+```
+Et la commande
+```
+yarnkpg add @fortawesome/fontawesome-free
+```
+
+## Effectuer les migrations 
+```
+./manage.py makemigrations Gueze
+```
+```
+./manage.py sqlmigrate Gueze 0001
+```
+```
+./manage.py migrate
+```
+
+## Lancer l'application
+Se dirigier dans la source du projet : /Djangolic/
+Taper la commande :
+```
+./manage.py runserver
+```
+Aller sur votre navigateur et écrire le lien : 127.0.0.1/task_and_user
+
+
+
+
+
+
+
