9d164c3211b97685dc7bfedb6c299fc4565f9ac7
Théo Gicquel <theo.gicquel.work@gmail.com>
2022-11-26 15:36:32 +0100

    :bug: cleanup model

--- Gueze/models.py
+++ Gueze/models.py
@@ -7,31 +7,12 @@ class Glass(models.Model):
     def __unicode__(self):
         return self.name
 
-'''
-class glass(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100)
-    description = models.CharField(max_length=5000)
-    asset_link = models.CharField(max_length=5000)
 
-    def __unicode__(self):
-        return self.name
-'''
-class HexColor(models.Model):
-    hex = models.CharField(max_length=10)
 
-    def __unicode__(self):
-        return self.hex
 
-class srm(models.Model):
-    id = models.AutoField(primary_key=True)
-    color = models.CharField(max_length=100)
-    hex = models.ForeignKey(HexColor, on_delete=models.CASCADE)
 
-    def __unicode__(self):
-        return self.color
 
-class location(models.Model):
+class Location(models.Model):
     id = models.AutoField(primary_key=True)
 
     name = models.CharField(max_length=100)
@@ -42,14 +23,14 @@ class location(models.Model):
 
 
 
-class brewery(models.Model):
+class Brewery(models.Model):
     id = models.AutoField(primary_key=True)
 
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=5000)
     website = models.CharField(max_length=5000)
     update_at = models.DateField()
-    location_brewery = models.ForeignKey(location, on_delete=models.CASCADE)
+    location_brewery = models.ForeignKey(Location, on_delete=models.CASCADE)
 
     def __unicode__(self):
         return self.name
