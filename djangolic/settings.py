c6871f389b9f9d820e3dea1acb742974bb68a747
Théo Gicquel <theo.gicquel.work@gmail.com>
2022-11-27 15:08:04 +0100

    :closed_lock_with_key: setup .env

--- djangolic/settings.py
+++ djangolic/settings.py
@@ -1,3 +1,5 @@
+import os
+import environ
 """
 Django settings for djangolic project.
 
@@ -15,17 +17,21 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env()
+environ.Env.read_env()
+# add path to windows npm
+if os.name == 'nt':
+    NPM_BIN_PATH = env('NPM_BIN_PATH_WINDOWS')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2%vc+(+w5m-=2vj8-ws%z&czhwji@j6*vzvt5q9zv=n3ktvp62'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
 
 
 # Application definition
