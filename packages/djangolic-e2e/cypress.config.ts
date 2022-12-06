11e9343fa7078f9ba0361cf97b62bab1eebbe7c2
theogicquel <theo.gicquel.work@gmail.com>
2022-12-06 12:17:48 +0100

    change djangolic-e2e to use typescript 🏗️

--- packages/djangolic-e2e/cypress.config.ts
+++ packages/djangolic-e2e/cypress.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'cypress';
 import { nxE2EPreset } from '@nrwl/cypress/plugins/cypress-preset';
+
 export default defineConfig({
+  projectId: 'j7dkfr',
   e2e: nxE2EPreset(__dirname),
 });
