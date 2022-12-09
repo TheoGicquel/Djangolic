5b23e588d9dd1625ebceda5bdcfa973e559fd7b0
Théo Gicquel <theo.gicquel.work@gmail.com>
2022-12-09 20:51:14 +0100

    hotfix: djangolic gui & missing python dependencies

--- packages/djangolic-e2e/cypress.config.ts
+++ packages/djangolic-e2e/cypress.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'cypress';
 import { nxE2EPreset } from '@nrwl/cypress/plugins/cypress-preset';
 
 export default defineConfig({
-  projectId: 'j7dkfr',
-  e2e: nxE2EPreset(__dirname),
+  e2e: {
+    ...nxE2EPreset(__dirname),
+    experimentalRunAllSpecs: true,
+  },
 });
