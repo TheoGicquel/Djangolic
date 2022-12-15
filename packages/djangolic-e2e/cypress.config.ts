0e7ff2711d09e210dd521cdf70dab301e5257f17
theogicquel <theo.gicquel.work@gmail.com>
2022-12-15 21:16:16 +0100

    updated cypress config (reduced timeout, added cloud sync )

--- packages/djangolic-e2e/cypress.config.ts
+++ packages/djangolic-e2e/cypress.config.ts
@@ -2,11 +2,11 @@ import { defineConfig } from 'cypress';
 import { nxE2EPreset } from '@nrwl/cypress/plugins/cypress-preset';
 
 export default defineConfig({
+  projectId: 'j7dkfr',
   e2e: {
     ...nxE2EPreset(__dirname),
     experimentalRunAllSpecs: true,
     baseUrl: 'http://127.0.0.1:8000',
-    defaultCommandTimeout: 10000,// prob overkill but github actions is slow
     retries: 2,
     video: false,
     screenshotOnRunFailure: false,
